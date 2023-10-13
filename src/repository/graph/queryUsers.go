e451740454d780ae7877899a754d0d7959025ff0
Björn Eberhardt <beberhar@thoughtworks.com>
2023-10-13 21:25:06 +0200

    Find users administered by another user, necessary to post or edit trainings or comments on their behalf

--- src/repository/graph/queryUsers.go
+++ src/repository/graph/queryUsers.go
@@ -30,3 +30,32 @@ func (db *Db) GetAllUsers(ctx context.Context) ([]domain.User, error) {
 
 	return result, nil
 }
+func (db *Db) GetAssumableUsers(key string, ctx context.Context) ([]domain.User, error) {
+
+	// user{_key: @key} -[administers*0..]->user
+	query := "FOR doc IN users\n"
+	query += "  FILTER doc._key == @key\n"
+	query += "  FOR v, e IN 0..99 OUTBOUND doc edges\n"
+	query += "    PRUNE e != null && e.label != \"administers\""
+	query += "    FILTER e == null || e.label == \"administers\" RETURN DISTINCT v"
+
+	cursor, err := db.Database.Query(ctx, query, map[string]interface{}{"key": key})
+	if err != nil {
+		return nil, fmt.Errorf("query string invalid: %w", err)
+	}
+	defer cursor.Close()
+
+	var result []domain.User
+	for {
+		var doc domain.User
+		_, err := cursor.ReadDocument(ctx, &doc)
+		if driver.IsNoMoreDocuments(err) {
+			break
+		} else if err != nil {
+			return nil, fmt.Errorf("obtaining documents failed: %w", err)
+		}
+		result = append(result, doc)
+	}
+
+	return result, nil
+}
---
+++ src/repository/graph/queryUsers_test.go
@@ -0,0 +1,91 @@
+package graph
+
+import (
+	"pkv/api/src/domain"
+	"testing"
+)
+
+func TestGetAssumableUsers(t *testing.T) {
+	db, _, err := Init("../../../config.yml", true)
+	if err != nil {
+		t.Fatalf("db initialisation failed: %s", err)
+	}
+	users := []domain.User{
+		{Key: "admin"},
+		{Key: "u1"},
+		{Key: "u11"},
+		{Key: "u12"},
+		{Key: "decoy1"},
+		{Key: "decoy2"},
+		{Key: "decoy3"},
+	}
+	for _, user := range users {
+		err = db.Users.Create(&user, nil)
+		if err != nil {
+			t.Fatalf("user creation failed: %s", err)
+		}
+	}
+	err = db.UserAdministersUser(users[0], users[1], nil)
+	if err != nil {
+		t.Fatalf("linking user to user failed: %s", err)
+	}
+	err = db.UserAdministersUser(users[1], users[2], nil)
+	if err != nil {
+		t.Fatalf("linking user to user failed: %s", err)
+	}
+	err = db.UserAdministersUser(users[1], users[3], nil)
+	if err != nil {
+		t.Fatalf("linking user to user failed: %s", err)
+	}
+	err = db.UserAdministersUser(users[2], users[3], nil)
+	if err != nil {
+		t.Fatalf("linking user to user failed: %s", err)
+	}
+	if _, err := db.Edges.CreateDocument(nil, domain.Edge{
+		From:  "users/" + users[0].Key,
+		To:    "users/" + users[4].Key,
+		Label: "decoys",
+	}); err != nil {
+		t.Fatalf("Link failed: %s", err)
+	}
+	if _, err := db.Edges.CreateDocument(nil, domain.Edge{
+		From:  "users/" + users[4].Key,
+		To:    "users/" + users[5].Key,
+		Label: "decoys",
+	}); err != nil {
+		t.Fatalf("Link failed: %s", err)
+	}
+	if _, err := db.Edges.CreateDocument(nil, domain.Edge{
+		From:  "users/" + users[4].Key,
+		To:    "users/" + users[6].Key,
+		Label: "administers",
+	}); err != nil {
+		t.Fatalf("Link failed: %s", err)
+	}
+
+	assumableUsers, err := db.GetAssumableUsers(users[0].Key, nil)
+	if err != nil {
+		t.Fatalf("get assumable users failed: %s", err)
+	}
+	var assumableUserKeys []string
+	for _, user := range assumableUsers {
+		assumableUserKeys = append(assumableUserKeys, user.Key)
+	}
+	if len(assumableUserKeys) != 4 {
+		t.Fatalf("expected 4 users, got %v", assumableUserKeys)
+	}
+	// compare with users[0:3] ignoring order
+	for _, user := range users[0:3] {
+		if !contains(assumableUserKeys, user.Key) {
+			t.Fatalf("expected %s to be assumable, got %v", user.Key, assumableUserKeys)
+		}
+	}
+}
+func contains(slice []string, element string) bool {
+	for _, e := range slice {
+		if e == element {
+			return true
+		}
+	}
+	return false
+}
