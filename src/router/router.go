bd60dc34e1e70713e79b9bf44d552376467ee240
Björn Eberhardt <beberhar@thoughtworks.com>
2024-07-16 00:43:07 +0200

    add altcha to CORS headers

--- src/router/router.go
+++ src/router/router.go
@@ -75,7 +75,7 @@ func NewServer(configPath string, test bool) *http.Server {
 	r.GlobalOPTIONS = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		if r.Header.Get("Access-Control-Request-Method") != "" {
 			header := w.Header()
-			header.Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
+			header.Set("Access-Control-Allow-Headers", "Content-Type, Authorization, x-altcha-spam-filter")
 			header.Set("Access-Control-Allow-Methods", header.Get("Allow"))
 			header.Set("Access-Control-Allow-Origin", r.Header.Get("Origin"))
 		}
